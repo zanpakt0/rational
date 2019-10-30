ea90951c9bc5fddef093d9155f5adae16441d1dd
zanpakt0 <42385113+zanpakt0@users.noreply.github.com>
2019-10-30 17:40:47 +0200

    Add test class

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,4 +8,35 @@ public class Tests {
         assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
+
+    @Test
+    public void testNotEmptyConstructor() {
+        Rational standard = new Rational(1, 2);
+        assertEquals("Standard constructor returns wrong numerator", 1, standard.getNumerator());
+        assertEquals("Standard constructor returns wrong denominator", 2, standard.getDenominator());
+    }
+
+    @Test
+    public void testDivisionZeroInConstructor() {
+    	try {
+    		Rational standard = new Rational(1, 0);
+    		fail("Expected ArithmeticException");
+    	} catch(ArithmeticException ex){
+    		assertEquals("Wrong Assert", "division by zero !", ex.getMessage());
+    	}
+    }
+
+    @Test
+    public void testSetNumerator() {
+        Rational standard = new Rational();
+        standard.setNumerator(1);
+        assertEquals("Standard constructor returns wrong numerator", 1, standard.getNumerator());
+    }
+
+    @Test
+    public void testSetDenominator() {
+        Rational standard = new Rational(1,1);
+        standard.setDenominator(2);
+        assertEquals("Standard constructor returns wrong denominator", 2, standard.getDenominator());
+    }
 }
